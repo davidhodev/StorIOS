7b91b20e74bf663ad52a30bed69993e52cb58cdd
Cole-Stor <cdavis3912@gmail.com>
2018-05-24 16:20:20 -0700

    fixed menu to new view controller

--- Stor/MenuViewController.swift
+++ Stor/MenuViewController.swift
@@ -2,7 +2,7 @@
 //  MenuViewController.swift
 //  Stor
 //
-//  Created by Cole Feldman on 5/23/18.
+//  Created by Cole Feldman on 5/24/18.
 //  Copyright © 2018 David Ho. All rights reserved.
 //
 
@@ -10,6 +10,9 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBAction func exitButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +24,4 @@ class MenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
