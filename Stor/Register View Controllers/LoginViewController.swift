245a12722d21d70d7753a0dc728db7c2f4f36995
Cole-Stor <cdavis3912@gmail.com>
2018-05-25 12:37:13 -0700

    incorrect imput animations on login page

--- Stor/Register View Controllers/LoginViewController.swift
+++ Stor/Register View Controllers/LoginViewController.swift
@@ -17,8 +17,10 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var line1Image: UIImageView!
     // instantiate eye button paramter
     var iconClick : Bool!
-    //change color animation function
-
+    // instantiate letter image
+    @IBOutlet weak var letterImage: UIImageView!
+    // instantiate lock image
+    @IBOutlet weak var lockImage: UIImageView!
     
     var activityIndicator:UIActivityIndicatorView = UIActivityIndicatorView()
     
@@ -50,6 +52,8 @@ class LoginViewController: UIViewController {
         iconClick = true
         line1Image.image = UIImage.init(named: "Line 2")
         line2Image.image = UIImage.init(named: "Line 2")
+        letterImage.image = UIImage.init(named: "Combined Shape1")
+        lockImage.image = UIImage.init(named: "Combined Shape2")
         // Do any additional setup after loading the view.
     }
 
@@ -79,22 +83,46 @@ class LoginViewController: UIViewController {
                     switch errCode {
                     case .userNotFound:
                         print("No Email Found")
-                         self.emailText.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                        // switches email bar to red
+                        self.emailText.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                        // removes text
                         self.emailText.text = ""
                         self.passwordText.text = ""
+                        // sets bar and icon to red
                         self.line1Image.image = UIImage.init(named: "Line 2Red")
+                        self.letterImage.image = UIImage.init(named: "Mail Icon Red")
+                        // sets other bars and icons to original
+                        self.passwordText.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(white: 0.44, alpha: 0.56)])
+                        self.line2Image.image = UIImage.init(named: "Line 2")
+                        self.lockImage.image = UIImage.init(named: "Combined Shape2")
                     case .invalidEmail:
+                        // switches placeholder to red
                         print("Invalid email")
                         self.emailText.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                        // removes text
                         self.emailText.text = ""
                         self.passwordText.text = ""
+                        // sets placeholder, bar, and icon to red
                         self.line1Image.image = UIImage.init(named: "Line 2Red")
+                        self.letterImage.image = UIImage.init(named: "Mail Icon Red")
+                        // sets other bars and icons to original
+                        self.passwordText.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(white: 0.44, alpha: 0.56)])
+                        self.line2Image.image = UIImage.init(named: "Line 2")
+                        self.lockImage.image = UIImage.init(named: "Combined Shape2")
                     case .wrongPassword:
                         print("Incorrect password")
+                        // sets placeholder red
                         self.passwordText.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                        //removes text
                         self.emailText.text = ""
                         self.passwordText.text = ""
+                        // sets bars and icons to red
                         self.line2Image.image = UIImage.init(named: "Line 2Red")
+                        self.lockImage.image = UIImage.init(named: "Red Lock")
+                        // reverts other bar and icon
+                        self.emailText.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(white: 0.44, alpha: 0.56)])
+                        self.line1Image.image = UIImage.init(named: "Line 2")
+                        self.letterImage.image = UIImage.init(named: "Combined Shape1")
                     default:
                         print("Create User Error: \(error)")
                     }
