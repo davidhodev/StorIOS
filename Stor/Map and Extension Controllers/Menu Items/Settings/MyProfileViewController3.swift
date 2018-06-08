79d4961d0872ce7e76eb5035842ae7b3396d56e5
cole-stor <cdavis3912@gmail.com>
2018-06-08 15:52:19 -0700

    fixed myprofile

---
+++ Stor/Map and Extension Controllers/Menu Items/Settings/MyProfileViewController3.swift
@@ -0,0 +1,38 @@
+//
+//  MyProfileViewController3.swift
+//  Stor
+//
+//  Created by Cole Feldman on 6/8/18.
+//  Copyright © 2018 David Ho. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
