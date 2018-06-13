b60c26abc763502a0904cd773f2fff30474882aa
davidhodev <davidhodev@gmail.com>
2018-06-13 12:42:25 -0700

    Profile has database info and provider viewcontrollers made

--- Stor/Map and Extension Controllers/Menu Items/Settings/MyProfileViewController3.swift
+++ Stor/Map and Extension Controllers/Menu Items/Settings/MyProfileViewController3.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class MyProfileViewController3: UIViewController {
 
+    @IBOutlet weak var informationButton: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.nameLabel.text = globalVariablesViewController.username
+        print("ralet sting", String(describing: globalVariablesViewController.ratingNumber))
+        let outputRating2 = ((globalVariablesViewController.ratingNumber as! Double) * 100).rounded()/100
+        self.ratingLabel.text = String(format: "%.2f",  outputRating2)
+        
+        //Hexagon SHape
+        let lineWidth = CGFloat(7.0)
+        let rect = CGRect(x: 0, y: 0.0, width: 70, height: 76)
+        let sides = 6
+        
+        let path = roundedPolygonPath(rect: rect, lineWidth: lineWidth, sides: sides, cornerRadius: 8.0, rotationOffset: CGFloat(.pi / 2.0))
+        
+        let borderLayer = CAShapeLayer()
+        borderLayer.frame = CGRect(x : 0.0, y : 0.0, width : path.bounds.width + lineWidth, height : path.bounds.height + lineWidth)
+        borderLayer.path = path.cgPath
+        borderLayer.lineWidth = lineWidth
+        borderLayer.lineJoin = kCALineJoinRound
+        borderLayer.lineCap = kCALineCapRound
+        borderLayer.strokeColor = UIColor.black.cgColor
+        borderLayer.fillColor = UIColor.white.cgColor
+        
+        _ = createImage(layer: borderLayer)
+        
+        
+        
+        profileImage.contentMode = .scaleAspectFill
+        //        profileImage.layer.cornerRadius = 20
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.mask = borderLayer
+        //        profileImage.layer.addSublayer(borderLayer)
+        profileImage.isUserInteractionEnabled = true
+//        profileImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectImageView)))
+        
+        profileImage.loadProfilePicture()
         // Do any additional setup after loading the view.
     }
 
@@ -24,15 +61,14 @@ class MyProfileViewController3: UIViewController {
     @IBAction func BackButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func informationButtonPressed(_ sender: Any) {
+        [UIButton .animate(withDuration: 0.3, animations: {
+            self.informationButton.alpha = 0
+        })]
     }
-    */
+    
+    
+    
 
 }
