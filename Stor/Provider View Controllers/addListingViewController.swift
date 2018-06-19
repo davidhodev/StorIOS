27c141b61223d88fb3ad6173fceb35d1c8b80884
davidhodev <davidhodev@gmail.com>
2018-06-19 16:44:27 -0700

    Add Listing

--- Stor/Provider View Controllers/addListingViewController.swift
+++ Stor/Provider View Controllers/addListingViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class addListingViewController: UIViewController {
 
-
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
     @IBAction func addListingButton(_ sender: Any) {
         print("add Listing!")
     }
@@ -20,6 +25,53 @@ class addListingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let user = Auth.auth().currentUser{
+            Database.database().reference().child("Providers").child(user.uid).child("personalInfo").observe(.value, with: { (snapshot) in
+                if let dictionary = snapshot.value as? [String: Any]{
+                    let ratingString = String(describing: dictionary["rating"]!)
+                    let roundedRating = (Double(ratingString)! * 100).rounded()/100
+                    let ratingTemp = String(format: "%.2f", roundedRating)
+                    // MEDIUM
+                    let fontRating: UIFont? = UIFont(name: "Dosis-Medium", size:14)
+                    let ratingAttString:NSMutableAttributedString = NSMutableAttributedString(string: ratingTemp, attributes: [.font: fontRating!])
+                    self.ratingLabel.attributedText = ratingAttString
+                    
+                    let nameString = String(describing: dictionary["Name"]!)
+                    let nameFont: UIFont? = UIFont(name: "Dosis-Bold", size:18)
+                    let nameAttString:NSMutableAttributedString = NSMutableAttributedString(string: nameString, attributes: [.font: nameFont!])
+                    self.nameLabel.attributedText = nameAttString
+                    
+                }
+            })
+        }
+        
+        
+        //Hexagon SHape
+        let lineWidth = CGFloat(7.0)
+        let rect = CGRect(x: 0, y: 0.0, width: 90, height: 96)
+        let sides = 6
+        
+        let path = roundedPolygonPath(rect: rect, lineWidth: lineWidth, sides: sides, cornerRadius: 8.0, rotationOffset: CGFloat(.pi / 2.0))
+        
+        let borderLayer = CAShapeLayer()
+        borderLayer.frame = CGRect(x : 0.0, y : 0.0, width : path.bounds.width + lineWidth, height : path.bounds.height + lineWidth)
+        borderLayer.path = path.cgPath
+        borderLayer.lineWidth = lineWidth
+        borderLayer.lineJoin = kCALineJoinRound
+        borderLayer.lineCap = kCALineCapRound
+        borderLayer.strokeColor = UIColor.black.cgColor
+        borderLayer.fillColor = UIColor.white.cgColor
+        
+        _ = createImage(layer: borderLayer)
+        
+        
+        
+        profileImage.contentMode = .scaleAspectFill
+        //        profileImage.layer.cornerRadius = 20
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.mask = borderLayer
+        profileImage.loadProfilePicture()
 
         // Do any additional setup after loading the view.
     }
--- Stor/Provider View Controllers/myListingViewController.swift
+++ Stor/Provider View Controllers/myListingViewController.swift
@@ -32,16 +32,16 @@ class myListingViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     @IBAction func addListingButton(_ sender: Any) {
-        if exists! == true{
-            let alert = UIAlertController(title: "Uh-oh", message: "Looks like you already having a listing out! You are limitted to only 1 listing at a time. We apologize for the inconvenience. More listings will be allowed in the next update!", preferredStyle: .alert)
-            self.present(alert, animated: true, completion:{
-                alert.view.superview?.isUserInteractionEnabled = true
-                alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertControllerBackgroundTapped)))
-            })
-        }
-        else{
+//        if exists! == true{
+//            let alert = UIAlertController(title: "Uh-oh", message: "Looks like you already having a listing out! You are limitted to only 1 listing at a time. We apologize for the inconvenience. More listings will be allowed in the next update!", preferredStyle: .alert)
+//            self.present(alert, animated: true, completion:{
+//                alert.view.superview?.isUserInteractionEnabled = true
+//                alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertControllerBackgroundTapped)))
+//            })
+//        }
+//        else{
             performSegue(withIdentifier: "addListingSegue", sender: self)
-        }
+//        }
     }
     
     
