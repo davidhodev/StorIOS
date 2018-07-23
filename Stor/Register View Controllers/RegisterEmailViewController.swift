38cca540ff4595dae9ef1b0e189c3375e54319b8
davidhodev <davidhodev@gmail.com>
2018-07-23 15:00:03 -0700

    Create Account

--- Stor/Register View Controllers/RegisterEmailViewController.swift
+++ Stor/Register View Controllers/RegisterEmailViewController.swift
@@ -33,29 +33,171 @@ class RegisterEmailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailRegisterText: UITextField!
     @IBOutlet weak var passwordRegisterText: UITextField!
     @IBOutlet weak var phoneRegisterText: UITextField!
+    @IBOutlet weak var confirmPasswordRegisterText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    @IBOutlet weak var phoneVerificationText: UITextField!
+    
+    
     @IBOutlet var registerView: UIView!
     
-    //line under email address
-    @IBOutlet weak var lineEmail: UIImageView!
-    //line under phone number
-    @IBOutlet weak var linePhone: UIImageView!
-    //line under password
-    @IBOutlet weak var linePassword: UIImageView!
-    //email picture
-    @IBOutlet weak var emailPicture: UIImageView!
-    //phone picture
-    @IBOutlet weak var phonePicture: UIImageView!
-    //password picture
-    @IBOutlet weak var passwordPicture: UIImageView!
+    var registerSteps = 0
     
-    // Button for Register
-    @IBAction func registerButton(_ sender: Any) {
-        self.register()
+    //line under email address
+    @IBOutlet weak var line1: UIImageView!
+    //line under phone number
+    @IBOutlet weak var line2: UIImageView!
+    //line under password
+    @IBOutlet weak var line3: UIImageView!
+    //Main Picture
+    @IBOutlet weak var mainImage: UIImageView!
+    //Password Pciture
+    @IBOutlet weak var passwordImage: UIImageView!
+    //Confirm PasswordPicture
+    @IBOutlet weak var confirmPasswordImage: UIImageView!
+    
+    
+    //Question Label
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    
+    // Next Button
+    @IBOutlet weak var nextButtonOutlet: UIButton!
+    @IBAction func nextButton(_ sender: Any) {
+//        registerSteps += 1
+        if registerSteps == 0{
+            self.nameRegisterText.alpha = 0
+            self.emailRegisterText.alpha = 1
+            self.mainImage.image =  UIImage.init(named: "Combined Shape1")
+            self.questionLabel.text = "What's your email?"
+            registerSteps += 1
+            // Animation to bring in Email
+        }
+        else if registerSteps == 1{
+            Auth.auth().fetchProviders(forEmail: emailRegisterText.text!, completion: { (stringArray, error) in
+                if error != nil{
+                    print(error)
+                    self.line1.image = UIImage.init(named: "Line 2Red")
+                    self.emailRegisterText.attributedPlaceholder = NSAttributedString(string: "E-mail is not in proper format", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                    self.emailRegisterText.text = ""
+                    self.mainImage.image = UIImage.init(named: "Mail Icon Red")
+                }
+                else{
+                    if stringArray == nil{
+                        self.emailRegisterText.alpha = 0
+                        self.passwordRegisterText.alpha = 1
+                        self.confirmPasswordRegisterText.alpha = 1
+                        self.mainImage.alpha = 0
+                        self.passwordImage.alpha = 1
+                        self.confirmPasswordImage.alpha = 1
+                        
+                        self.line1.alpha = 0
+                        self.line2.alpha = 1
+                        self.line3.alpha = 1
+                        
+                        self.questionLabel.text = "What's your password?"
+                        self.registerSteps += 1
+                    }
+                    else{
+                        self.line1.image = UIImage.init(named: "Line 2Red")
+                        self.emailRegisterText.attributedPlaceholder = NSAttributedString(string: "Acount already exists with that email", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                        self.emailRegisterText.text = ""
+                        self.mainImage.image = UIImage.init(named: "Mail Icon Red")
+                        print("USER EXISTS WITH THAT EMAIL")
+                    }
+                }
+            })
+        }
+        else if registerSteps == 2{
+            let characterset = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+            
+            
+            if passwordRegisterText.text! != confirmPasswordRegisterText.text!{
+                self.confirmPasswordRegisterText.attributedPlaceholder = NSAttributedString(string: "Your passwords don't match!", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                self.confirmPasswordRegisterText.text = ""
+                self.confirmPasswordImage.image = UIImage.init(named: "Red Lock")
+                self.line3.image = UIImage.init(named: "Line 2Red")
+                print("Passwords don't match!")
+            }
+            else if (passwordRegisterText.text?.count)! < 6{
+                self.passwordRegisterText.attributedPlaceholder = NSAttributedString(string: "Must be at least 6 characters", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                self.confirmPasswordRegisterText.attributedPlaceholder = NSAttributedString(string: "Must be at least 6 characters", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                self.passwordRegisterText.text = ""
+                self.confirmPasswordRegisterText.text = ""
+                self.passwordImage.image = UIImage.init(named: "Red Lock")
+                self.confirmPasswordImage.image = UIImage.init(named: "Red Lock")
+                self.line2.image = UIImage.init(named: "Line 2Red")
+                self.line3.image = UIImage.init(named: "Line 2Red")
+                
+                print ("Password must at least be 6 characters")
+            }
+            else if passwordRegisterText.text?.rangeOfCharacter(from: characterset.inverted) == nil{
+                self.passwordRegisterText.attributedPlaceholder = NSAttributedString(string: "Must have 1 special character", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                self.confirmPasswordRegisterText.attributedPlaceholder = NSAttributedString(string: "Must have 1 special character", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
+                self.passwordRegisterText.text = ""
+                self.confirmPasswordRegisterText.text = ""
+                self.passwordImage.image = UIImage.init(named: "Red Lock")
+                self.confirmPasswordImage.image = UIImage.init(named: "Red Lock")
+                self.line2.image = UIImage.init(named: "Line 2Red")
+                self.line3.image = UIImage.init(named: "Line 2Red")
+                
+                print("String has no Special Characters")
+            }
+            else{
+                self.birthdayText.alpha = 1
+                self.passwordRegisterText.alpha = 0
+                self.confirmPasswordRegisterText.alpha = 0
+                self.mainImage.image =  UIImage.init(named: "Combined Shape1") // Birthday Icon
+                self.mainImage.alpha = 1
+                self.passwordImage.alpha = 0
+                self.confirmPasswordImage.alpha = 0
+                
+                self.line1.alpha = 1
+                self.line2.alpha = 0
+                self.line3.alpha = 0
+                
+                self.questionLabel.text = "What's your Birthday? (Must be at least 13 to use the app)"
+                self.registerSteps += 1
+            }
+        }
+        else if registerSteps == 3{
+            /*
+             if birthdayCheck < 13{
+                print("Yo Birthday must be at least 13")
+             }
+             else{
+             
+             }
+ */
+            self.birthdayText.alpha = 0
+            self.phoneRegisterText.alpha = 1
+            self.mainImage.image =  UIImage.init(named: "Phone Icon")
+            self.questionLabel.text = "What's your phone Number?"
+            registerSteps += 1
+        }
+        else if registerSteps == 4{
+            
+        }
+        
+        
+        if registerSteps == 5{
+            self.register()
+        }
+        
+        
     }
-    // takes you back to the front page
-    @IBAction func registrationBackButton(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+    
+    @IBOutlet weak var backButtonOutlet: UIButton!
+    @IBAction func backButton(_ sender: Any) {
+        
+        if registerSteps == 0{
+            self.navigationController?.popToRootViewController(animated: true)
+        }
+//        registerSteps -= 1
+        
     }
+    
+    
     
     
     
@@ -71,13 +213,28 @@ class RegisterEmailViewController: UIViewController, UITextFieldDelegate {
         phoneRegisterText.delegate = self
         passwordRegisterText.delegate = self
         
+        // Setting Alphas to 0
+        emailRegisterText.alpha = 0
+        passwordRegisterText.alpha = 0
+        confirmPasswordRegisterText.alpha = 0
+        birthdayText.alpha = 0
+        phoneRegisterText.alpha = 0
+        phoneVerificationText.alpha = 0
+        line2.alpha = 0
+        line3.alpha = 0
+        passwordImage.alpha = 0
+        confirmPasswordImage.alpha = 0
+        questionLabel.text = "What's your name?"
         
-        lineEmail.image = UIImage.init(named: "Line 2")
-        linePhone.image = UIImage.init(named: "Line 2")
-        linePassword.image = UIImage.init(named: "Line 2")
-        emailPicture.image = UIImage.init(named: "Combined Shape1")
-        phonePicture.image = UIImage.init(named: "Phone Icon")
-        passwordPicture.image = UIImage.init(named: "Combined Shape2")
+        
+        
+        
+        line1.image = UIImage.init(named: "Line 2")
+        line2.image = UIImage.init(named: "Line 2")
+        line3.image = UIImage.init(named: "Line 2")
+        mainImage.image = UIImage.init(named: "nameCardIcon")
+//        phonePicture.image = UIImage.init(named: "Phone Icon")
+        passwordImage.image = UIImage.init(named: "Combined Shape2")
         
         self.hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
@@ -105,10 +262,10 @@ class RegisterEmailViewController: UIViewController, UITextFieldDelegate {
             print("Valid Phone number")
         }
         else{
-            self.linePhone.image = UIImage.init(named: "Line 2Red")
+            self.line2.image = UIImage.init(named: "Line 2Red")
             self.phoneRegisterText.attributedPlaceholder = NSAttributedString(string: "Phone number is not valid", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
             self.phoneRegisterText.text = ""
-            self.phonePicture.image = UIImage.init(named: "Phone Icon Red")
+//            self.phonePicture.image = UIImage.init(named: "Phone Icon Red")
             return
         }
         
@@ -148,28 +305,28 @@ class RegisterEmailViewController: UIViewController, UITextFieldDelegate {
                     switch errCode {
                     case .invalidEmail: // E-mail is not in email format
                         print("Invalid email")
-                        self.lineEmail.image = UIImage.init(named: "Line 2Red")
+                        self.line1.image = UIImage.init(named: "Line 2Red")
                         self.emailRegisterText.attributedPlaceholder = NSAttributedString(string: "E-mail is not in proper format", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
                         self.emailRegisterText.text = ""
-                        self.emailPicture.image = UIImage.init(named: "Mail Icon Red")
+//                        self.emailPicture.image = UIImage.init(named: "Mail Icon Red")
                     case .weakPassword: //Weak Password, must be at least 6 characters long
                         print("Weak Password")
-                        self.linePassword.image = UIImage.init(named: "Line 2Red")
+                        self.line3.image = UIImage.init(named: "Line 2Red")
                         self.passwordRegisterText.attributedPlaceholder = NSAttributedString(string: "Must be at least 6 characters", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
                         self.passwordRegisterText.text = ""
-                        self.passwordPicture.image = UIImage.init(named: "Red Lock")
+//                        self.passwordPicture.image = UIImage.init(named: "Red Lock")
                     case .invalidPhoneNumber: // Phone number is not a valid phone number
                         print("Incorrect phone number")
-                        self.linePhone.image = UIImage.init(named: "Line 2Red")
+                        self.line2.image = UIImage.init(named: "Line 2Red")
                         self.phoneRegisterText.attributedPlaceholder = NSAttributedString(string: "Phone number is not valid", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
                         self.phoneRegisterText.text = ""
-                        self.phonePicture.image = UIImage.init(named: "Phone Icon Red")
+//                        self.phonePicture.image = UIImage.init(named: "Phone Icon Red")
                     case .emailAlreadyInUse: // Account Already exists with that email
                         print("account alrady exists")
-                        self.lineEmail.image = UIImage.init(named: "Line 2Red")
+                        self.line1.image = UIImage.init(named: "Line 2Red")
                         self.emailRegisterText.attributedPlaceholder = NSAttributedString(string: "Acount already exists with that email", attributes: [NSAttributedStringKey.foregroundColor: UIColor.init(red: 204/340, green: 17/340, blue: 119/340, alpha: 0.3)])
                         self.emailRegisterText.text = ""
-                        self.emailPicture.image = UIImage.init(named: "Mail Icon Red")
+//                        self.emailPicture.image = UIImage.init(named: "Mail Icon Red")
                     default:
                         print("Create User Error: \(error)")
                     }
